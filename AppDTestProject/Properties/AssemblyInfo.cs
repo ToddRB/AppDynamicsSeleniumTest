00996860c1fb2af8388be8e3197c280aef9d342d
Todd Bumbarger <bumbargertr@upmc.edu>
2021-01-14 13:07:39 -0500

    Initial add

---
+++ AppDTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppDTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppDTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1840f5f6-fb24-4c72-9982-b38e84b5f94f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AppDTestProject/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AppDTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        public IWebDriver Driver = null;
+
+        [ClassInitialize]
+        public static void TestClassInitialize(TestContext testContext)
+        {
+
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+
+            Driver = new ChromeDriver("c:\\temp\\", options);
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            Driver.Close();
+        }
+
+        [TestMethod]
+        public void ThisWillFailAfterRefreshingPage()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("https://www.appdynamics.com/");
+                var signInButton = Driver.FindElement(By.Id("dropdownMenu7-1"));
+
+                Driver.Navigate().Refresh();
+                signInButton.Click();
+
+                Console.WriteLine("I clicked the button!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+                Console.WriteLine(" I was unable to click the button =( ");
+                Teardown();
+            }
+        }
+
+        [TestMethod]
+        public void ThisWillPassAfterNotRefreshingPage()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("https://www.appdynamics.com/");
+                var signInButton = Driver.FindElement(By.Id("dropdownMenu7-1"));
+
+                signInButton.Click();
+
+                Console.WriteLine("I clicked the button!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("I was unable to click the button =(");
+                Teardown();
+            }
+        }
+    }
+}
